8d229012a44d520a833b1d3a7aac4b126fd133df
Alex Fish <fish@ustwo.co.uk>
2013-11-20 16:00:12 +0000

    Adding zshrc.sh file

---
+++ zsh/zshrc.sh
@@ -0,0 +1,26 @@
+# Path to your oh-my-zsh configuration.
+ZSH=$HOME/.oh-my-zsh
+
+# Set name of the theme to load.
+# Look in ~/.oh-my-zsh/themes/
+# Optionally, if you set this to "random", it'll load a random theme each
+# time that oh-my-zsh is loaded.
+ZSH_THEME="blinks"
+
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+alias ohmyzsh="st ~/.oh-my-zsh"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+plugins=(git sublime)
+
+source $ZSH/oh-my-zsh.sh
+
+# Customize to your needs...
+
+# Boxen
+if [ -f "/opt/boxen/env.sh" ]
+  then source "/opt/boxen/env.sh"
+fi
